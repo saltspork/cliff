1bed371bc3228afa2f8a3f40fc6978b296dac1f4
jkortman <james.kortman@gmail.com>
2020-01-27 13:14:58 +1030

    Added handling of incorrect command.

--- cliff.py
+++ cliff.py
@@ -18,7 +18,20 @@ async def on_ready():
 async def on_message(message):
     if client.user not in message.mentions:
         return
+
     logtime(f'Mentioned in: {message.content}')
+    channel = message.channel
+
+    # Parse message as a command
+    commands = message.content.split()[1:]
+    if commands[0] == 'list':
+        # TODO list active tickets
+        logtime('TODO List active tickets here')
+    elif commands[0] == 'resolve':
+        logtime('TODO resolve ticket here')
+    else:
+        await channel.send(f'unknown command `{commands[0]}`')
+
 
 def logtime(message):
     print(f'{datetime.datetime.now()} {message}')
